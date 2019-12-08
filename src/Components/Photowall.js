782c2665299566fa9db51d70705d23957264a89d
DrStupit <kerevreddy@hotmail.com>
2019-12-08 11:05:40 +0200

    [Add] Create route for adding images

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -3,19 +3,22 @@ import Photo from './Photo'
 import PropTypes from 'prop-types';
 
 // Statless function - Only when method contains only a render method...
-function Photowall (props) {
+function Photowall(props) {
   return (
-    <div className='photoGrid'>
-      {props.posts.map((post, index) => (
-        <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
-      ))}
+    <div>
+    <a onClick={props.onNavigate} href="#AddPhoto" className='addIcon'></a>
+      <div className='photoGrid'>
+        {props.posts.map((post, index) => (
+          <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
+        ))}
+      </div>
     </div>
   )
 }
 
 Photowall.propTypes = {
-    posts: PropTypes.array.isRequired,
-    onRemovePhoto: PropTypes.func.isRequired
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired
 }
 
 export default Photowall
